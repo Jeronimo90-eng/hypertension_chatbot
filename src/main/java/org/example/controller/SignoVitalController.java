b759b624c2dc8077c78e7dc21d46d34748395663
SIISA-TOC <jrobledoacevedo@gmail.com>
2026-03-24 22:57:37 -0500

    Resolved: Remove this unused import 'java.math.BigDecimal'

--- src/main/java/org/example/controller/SignoVitalController.java
+++ src/main/java/org/example/controller/SignoVitalController.java
@@ -5,7 +5,6 @@ import org.example.model.entity.SignoVital;
 import org.example.service.SignoVitalService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
